5bf69540a1a8e9a8399078bedd82025a20f8dacf
Dmitriy Solovev <dmptts@gmail.com>
2021-11-03 12:22:37 +0200

    Реализует проигрывание видео при наведении курсора на карточку фильма и его остановку

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -1,13 +1,37 @@
+import {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {Film} from '../../types/films';
+import {VideoPlayer} from '../video-player/video-player';
 
 type FilmCardProps = {
   film: Film;
   setActiveCard(id: number): void;
+  isActive: boolean;
 }
 
-function FilmCard ({film, setActiveCard}: FilmCardProps): JSX.Element {
-  const {id, name, previewImage} = film;
+function FilmCard ({film, setActiveCard, isActive}: FilmCardProps): JSX.Element {
+  const {id, name} = film;
+
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+
+  const videoDelay = 1;
+
+  useEffect(() => {
+    let delay: ReturnType<typeof setTimeout> | undefined;
+
+    if (isActive) {
+      delay = setTimeout(() => {
+        setIsVideoPlaying(true);
+      }, videoDelay * 1000);
+    }
+
+    return () => {
+      if (delay !== undefined) {
+        clearTimeout(delay);
+      }
+      setIsVideoPlaying(false);
+    };
+  }, [isActive]);
 
   return (
     <article
@@ -16,7 +40,7 @@ function FilmCard ({film, setActiveCard}: FilmCardProps): JSX.Element {
       onMouseOut={() => setActiveCard(0)}
     >
       <div className="small-film-card__image">
-        <img src={previewImage} alt={name} width="280" height="175" />
+        <VideoPlayer film={film} isVideoPlaying={isVideoPlaying} />
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`/films/${id}`}>{name}</Link>
--- project/src/components/video-player/video-player.tsx
+++ project/src/components/video-player/video-player.tsx
@@ -1,14 +1,29 @@
+import {useEffect, useRef} from 'react';
 import {Film} from '../../types/films';
 
 type VideoPlayerProps = {
   film: Film,
+  isVideoPlaying: boolean,
 }
 
-function VideoPlayer ({film}: VideoPlayerProps): JSX.Element {
-  const {videoLink} = film;
+function VideoPlayer ({film, isVideoPlaying}: VideoPlayerProps): JSX.Element {
+  const {videoLink, previewVideoLink} = film;
+
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    if (videoRef.current !== null) {
+      if (isVideoPlaying) {
+        videoRef.current.play();
+      } else {
+        videoRef.current.pause();
+        videoRef.current.currentTime = 0;
+      }
+    }
+  }, [isVideoPlaying]);
 
   return (
-    <video>
+    <video ref={videoRef} poster={previewVideoLink} muted >
       <source src={videoLink} type="video/mp4" />
     </video>
   );
